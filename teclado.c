54514e1b603e88a256dd973fdbf5dacf817eba46
20185189 <20185189@DSN1271158619.cfp127aluno.local>
2021-05-14 14:11:29 -0300

    Segundo commit. Projeto IHM, Modularização da função teclado

--- teclado.c
+++ teclado.c
@@ -19,8 +19,10 @@
 #define C3 PORTBbits.RB7
 #define MASK 0x10
 
-char tabela[2][4] = {{'1', '2' , '3' , 'A'},
-                     {'4', '5' , '6' , 'B'}};
+char tabela[4][4] = {{'1', '2' , '3' , 'A'},
+                     {'4', '5' , '6' , 'B'},
+                     {'7', '8' , '9' , 'C'},
+                     {'*', '0' , '#' , 'D'}};
 
 void teclado_init( void )
 {
@@ -32,14 +34,18 @@ void teclado_init( void )
 char teclado (void)
 {
     char tecla = 0;
-    char i;
+    char col, lin;
     
-    PORTB = 0x01;
-    for(i=0; i<4; i++) { if(PORTB & MASK<<i) tecla = tabela[0][i];}
+    for(lin=0; lin<4; lin++) //linha
+    {    
+        PORTB = 0x01<<lin;
+        for(col=0; col<4; col++) 
+        { 
+            if(PORTB & MASK<<col) 
+                tecla = tabela[lin][col];
+        }
+    }
     
-    PORTB = 0x02;
-    for(i=0; i<4; i++) { if(PORTB & MASK<<i) tecla = tabela[1][i];}
-
     return(tecla);     
 }    
     
